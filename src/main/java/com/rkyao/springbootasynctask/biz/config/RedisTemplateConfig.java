c7ed2929fabb3f1973f0d414dec5c29dc118eab8
rkyao <yao23711@163.com>
2024-06-01 20:19:42 +0800

    [mod] ValueSerializer

--- src/main/java/com/rkyao/springbootasynctask/biz/config/RedisTemplateConfig.java
+++ src/main/java/com/rkyao/springbootasynctask/biz/config/RedisTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.rkyao.springbootasynctask.biz.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -19,10 +20,11 @@ public class RedisTemplateConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
+        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(fastJsonRedisSerializer);
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
         return redisTemplate;
     }
 
